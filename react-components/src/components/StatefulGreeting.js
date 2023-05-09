23d29a9b6e98348f56b21afbad508e0ebd430ea0
Alain2020 <favouralain@yahoo.com>
2023-05-09 19:20:03 +0000

    commit inistals

--- react-components/src/components/StatefulGreeting.js
+++ react-components/src/components/StatefulGreeting.js
@@ -1,28 +1,30 @@
 import React from "react";
 
-export default class StatefulGreeting extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      count: 0
-    };
-  }
+class StatefulGreeting extends React.Component{
 
-  setCount = () => {
-    this.setState(prevState => {
-      return { count: prevState.count + 1 }
-    })
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            introduction: "Hello!",
+            buttonText: "Exit",
+        };
+    }
 
-  render() {
-    return (
-      <div className="greeting">
-        <h1>
-          Hello, {this.props.name}, I'm a stateful component!
-        </h1>
-        <h2>You've clicked {this.state.count} times</h2>
-        <button onClick={this.setCount}>Increment Count</button>
-      </div>
-    );
-  }
+    handleClick() {
+        this.setState({
+            introduction: "Goodbye!"
+        });
+        console.log(this.state.introduction);
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>{this.state.introduction} {this.props.greeting}</h1>
+                <button onClick={() => this.handleClick()}>{this.state.buttonText}</button>
+            </div>
+        )
+    }
 }
+
+export default StatefulGreeting;
